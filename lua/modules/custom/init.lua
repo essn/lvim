7d4cefa0aeccb56c77507ba2a976b2d375091bbf
Jesse Pledger <jessepledger@gmail.com>
2021-10-10 19:21:45 -0400

    Use OneDark color scheme

--- lua/modules/custom/init.lua
+++ lua/modules/custom/init.lua
@@ -2,7 +2,6 @@ local modules = {}
 
 -- Remove plugin from modules.global.init
 -- modules['kevinhwang91/rnvimr'] = false
-
 -- Rewrite plugin settings from modules.global.init
 -- modules['kevinhwang91/rnvimr'] = {
 --     your code
@@ -12,5 +11,12 @@ local modules = {}
 -- modules['sheerun/vim-polyglot'] = {
 --     your code
 -- }
+
+modules["lvim-tech/lvim-colorscheme"] = false
+
+modules['navarasu/onedark.nvim'] = {
+    event = "VimEnter",
+    config = [[vim.cmd('colorscheme onedark')]]
+}
 
 return modules
